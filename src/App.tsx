579acfa2d385dde58165ce07511cd01da292af72
Davi Marcilio <davimarcilio.js@gmail.com>
2023-01-11 18:51:03 -0300

    Header styled

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { ThemeProvider } from "styled-components";
+import { Transactions } from "./pages/Transactions";
 import { GlobalStyle } from "./styles/global";
 import { defaultTheme } from "./styles/themes/default";
 
@@ -6,7 +7,7 @@ export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
-      <h1>hello</h1>
+      <Transactions />
     </ThemeProvider>
   );
 }
