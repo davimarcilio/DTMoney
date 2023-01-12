1e08b1932522ae27f757fcdeaf8f7900165118fd
Davi Marcilio <davimarcilio.js@gmail.com>
2023-01-12 13:10:19 -0300

    New transactions modal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,5 @@
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
-
+import * as Dialog from "@radix-ui/react-dialog";
 import Logo from "../../assets/Logo.svg";
 
 export function Header() {
@@ -7,7 +7,19 @@ export function Header() {
     <HeaderContainer>
       <HeaderContent>
         <img src={Logo} alt="" />
-        <NewTransactionButton>Nova transação</NewTransactionButton>
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTransactionButton>Nova transação</NewTransactionButton>
+          </Dialog.Trigger>
+          <Dialog.Portal>
+            <Dialog.Overlay>
+              <Dialog.Content>
+                <Dialog.Title>Nova transação</Dialog.Title>
+                <Dialog.Close />
+              </Dialog.Content>
+            </Dialog.Overlay>
+          </Dialog.Portal>
+        </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
   );
